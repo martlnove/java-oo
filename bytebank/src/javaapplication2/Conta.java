46f06417ba9e2c13d4036129ac10951ffc5af810
Lucas Martins <lucasshc@gmail.com>
2022-05-13 14:57:14 -0300

    Criação de primeiras classes

---
+++ bytebank/src/javaapplication2/Conta.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package javaapplication2;
+
+/**
+ *
+ * @author Lenovo
+ */
+public class Conta {
+    double saldo;
+    int agencia;
+    int numero;
+    String titular;
+    
+}
