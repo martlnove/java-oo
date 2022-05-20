a6d39073a56e072687add462404d0e949bef2576
Lucas Martins <lucasshc@gmail.com>
2022-05-19 23:21:43 -0300

    Criação de construtores

--- byte-bank2/src/pkgbyte/bank2/Conta.java
+++ byte-bank2/src/pkgbyte/bank2/Conta.java
@@ -14,7 +14,13 @@ public class Conta {
     private int agencia;
     private int numero;
     private Cliente titular;
+    private static int total;
     
+    public Conta(int agencia, int numero){
+        Conta.total++;
+        this.agencia = agencia;
+        this.numero = numero;
+    }
     public void deposita(double valor){
         this.saldo += valor;
     
@@ -37,7 +43,7 @@ public class Conta {
         return false; 
     }   
     public double getSaldo(){
-    return this.saldo;
+        return this.saldo;
     }
     public int getNumero(){
         return this.numero;
@@ -51,5 +57,15 @@ public class Conta {
     }
     public void setAgencia(int agencia){
         this.agencia = agencia;
+    }
+    public void setTitular(Cliente titular){
+        this.titular = titular;
+    }
+    public Cliente getTitular(){
+        return titular;
+    }
+    
+    public static int getTotal(){
+        return Conta.total;
     }
 }
